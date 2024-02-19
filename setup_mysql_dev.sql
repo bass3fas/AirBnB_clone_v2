edeed270646cdba076a06064e14a18efa15c2c9e
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-02-19 02:36:28 -0800

    sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+--Create DataBase if doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+--Creat User if does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+--Grant Previliages to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
