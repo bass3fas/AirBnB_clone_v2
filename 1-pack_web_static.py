3dca07c787a7f7bbc89d7425b5c431ac4dc653f5
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-03-06 23:28:00 -0800

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder"""
+"""Fabric script that generates a .tgz
+archive from the contents of the web_static folder"""
 from fabric.api import local
 from datetime import datetime
 import os
@@ -7,7 +8,8 @@ import os
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    archive_path = 'versions/web_static_{}.tgz'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
+    archive_path = 'versions/web_static_{}.tgz'\
+        .format(datetime.now().strftime('%Y%m%d%H%M%S'))
 
     if not os.path.exists('versions'):
         os.makedirs('versions')
