fc2eb5a93e6f6c3cf6628f014da336f1b6e54ed4
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-03-06 23:22:00 -0800

    local instead of task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive from the contents of the web_static folder"""
-from fabric import task
+from fabric.api import local
 from datetime import datetime
 import os
 
 
-@task
 def do_pack():
-    archive_folder = 'web_static'
-    archive_name = 'web_static_{}.tgz'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
-    archive_path = 'versions/{}'.format(archive_name)
+    """Create a tar gzipped archive of the directory web_static."""
+    archive_path = 'versions/web_static_{}.tgz'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
 
     if not os.path.exists('versions'):
         os.makedirs('versions')
 
-    result = os.system('tar -cvzf {} {}'.format(archive_path, archive_folder))
+    result = local('tar -cvzf {} web_static'.format(archive_path))
 
-    if result != 0:
+    if result.return_code != 0:
         return None
     else:
         return archive_path
